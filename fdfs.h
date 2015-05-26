17819e37d0f0b48139fcd0ca76c232745b7c5725
yahan_lin <daimaldd@gmail.com>
2015-05-26 22:42:06 +0800

    update

--- fdfs.h
+++ fdfs.h
@@ -27,9 +27,9 @@
 
 typedef struct {
     char *msg; //当成功的时候,是返回图片的id,example:"group1/M00/00/00/wKgBP1NxvSqH9qNuAAAED6CzHYE179.jpg",当失败的时候是返回错误消息
-    int  result; //１表示成功,０表示失败
-}responseData;
+        int  result; //１表示成功,０表示失败
+        }responseData;
 
-
-responseData upload_file(char *conf_filename, char *local_filename);
-responseData delete_file(char *conf_filename, char *file_name);
+        responseData upload_file(char *conf_filename, char *local_filename);
+        responseData delete_file(char *conf_filename, char *file_name);
+}
